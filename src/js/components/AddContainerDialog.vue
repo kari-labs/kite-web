53864eba2e74b1470b7e418526e3b079ca202f1b
Joseph Zurowski <jzurowski2016@gmail.com>
2018-11-06 13:49:50 -0500

    Created Not Implemented Alert for WIP Functionality

--- src/js/components/AddContainerDialog.vue
+++ src/js/components/AddContainerDialog.vue
@@ -1,16 +1,17 @@
 <template>
-    <v-dialog v-model="createContainer" lazy max-width="500px">
+    <v-dialog v-model="showDialog" lazy max-width="500px">
         <v-btn class="mb-2" slot="activator" color="primary" dark>New Container</v-btn>
         <v-card>
             <v-card-title>
                 <span class="headline">New Container</span>
             </v-card-title>
             <v-divider/>
+            <not-implemented :alert="alertVisible"/>
             <v-card-text>
                 <v-container grid-list-md>
                     <v-layout wrap>
                         <v-flex xs12>
-                            <v-text-field v-model="blank" label="Student ID"/>
+                            <v-text-field v-model="studentID" label="Student ID"/>
                         </v-flex>
                     </v-layout>
                 </v-container>
@@ -18,8 +19,8 @@
             <v-divider/>
             <v-card-actions>
                 <v-spacer/>
-                <v-btn color="primary" flat @click.native="close">Cancel</v-btn>
-                <v-btn color="primary" flat @click.native="create">Create</v-btn>
+                <v-btn color="primary" flat @click.native="showDialog = false">Cancel</v-btn>
+                <v-btn color="primary" flat @click.native="createContainer">Create</v-btn>
             </v-card-actions>
         </v-card>
     </v-dialog>
@@ -27,10 +28,19 @@
 
 <script>
     export default {
-
+        methods: {
+            createContainer() {
+                console.log('Not implemented')
+                this.alertVisible = true
+            }
+        },
+        data: () => ({
+            studentID: '',
+            showDialog: false,
+            alertVisible: false
+        }),
+        components: {
+            'not-implemented': () => import(/* webpackChunkName: "notImplementedHelpers", webpackPrefetch: true */ './NotImplementedAlert.vue')
+        }
     }
 </script>
-
-<style>
-
-</style>
---
+++ src/js/components/NotImplementedAlert.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-alert :value="alert" type="error" icon="warning" transition="slide-y-transition">
+        Not Implemented
+    </v-alert>
+</template>
+
+<script>
+    export default {
+        props: {
+            alert: false
+        }
+    }
+</script>
+
+<style lang="stylus" scoped>
+    .v-alert.error
+        margin-top 0px
+</style>
