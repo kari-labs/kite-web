556dec2abece9257817a00d09c81eea1a039d991
Joseph Zurowski <jzurowski2016@gmail.com>
2018-11-06 17:17:32 -0500

    Cancelling Container Dialog Resets Form

--- src/js/components/AddContainerDialog.vue
+++ src/js/components/AddContainerDialog.vue
@@ -26,7 +26,7 @@
             <v-divider/>
             <v-card-actions>
                 <v-spacer/>
-                <v-btn color="primary" flat @click.native="showDialog = false">Cancel</v-btn>
+                <v-btn color="primary" flat @click.native="closeDialog">Cancel</v-btn>
                 <v-btn color="primary" flat :disabled="!formValid" @click.native="createContainer">Create</v-btn>
             </v-card-actions>
         </v-card>
@@ -40,6 +40,10 @@
                 if(this.$refs.form.validate()) {
                     this.implementVisible = true
                 }
+            },
+            closeDialog() {
+                this.showDialog = false
+                this.$refs.form.reset()
             }
         },
         data: () => ({
