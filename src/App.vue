27cb329d47364e0359c38ddc44e6d3051e81e126
Kyle <Arow411@gmail.com>
2018-11-08 15:01:09 -0500

    Converted to vue-cli

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <neit-header></neit-header>
+    <v-content id="content" class="scroll-y">
+        <router-view></router-view>
+    </v-content>
+    <neit-footer></neit-footer>
+  </div>
+</template>
+
+<script>
+  export default {
+    components: {
+      'neit-header': () => import(/* webpackChunkName: "initialLoad" */ '@/components/Header.vue'),
+      'neit-footer': () => import(/* webpackChunkName: "initialLoad" */ '@/components/Footer.vue')
+    }
+  }
+</script>
