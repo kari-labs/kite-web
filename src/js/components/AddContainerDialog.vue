7737e7c7e764c4d929967a1d6c7eb1b90ef8300a
Joseph Zurowski <jzurowski2016@gmail.com>
2018-11-07 20:36:41 -0500

    Added Progress Dialog for POSTs

--- src/js/components/AddContainerDialog.vue
+++ src/js/components/AddContainerDialog.vue
@@ -1,36 +1,40 @@
 <template>
-    <v-dialog v-model="showDialog" persistent lazy max-width="500px">
-        <v-btn class="mb-2" slot="activator" color="primary" dark>New Container</v-btn>
-        <v-card>
-            <v-card-title>
-                <span class="headline">New Container</span>
-            </v-card-title>
-            <v-divider/>
-            <error-alert :alert="implementVisible && formValid" text="Not implemented"/>
-            <error-alert :alert="!formValid" text="Please correct the errors and try again"/>
-            <v-card-text>
-                <v-container grid-list-md>
-                    <v-layout wrap>
-                        <v-flex xs12>
-                            <v-form ref="form" v-model="formValid" lazy-validation>
-                                <v-text-field 
-                                    v-model="studentID"
-                                    :rules="idRules"
-                                    label="Student ID"
-                                    required/>
-                            </v-form>
-                        </v-flex>
-                    </v-layout>
-                </v-container>
-            </v-card-text>
-            <v-divider/>
-            <v-card-actions>
-                <v-spacer/>
-                <v-btn color="primary" flat @click.native="closeDialog">Cancel</v-btn>
-                <v-btn color="primary" flat :disabled="!formValid" @click.native="createContainer">Create</v-btn>
-            </v-card-actions>
-        </v-card>
-    </v-dialog>
+    <div>
+        <v-dialog v-model="showDialog" persistent lazy max-width="500px">
+            <v-btn class="mb-2" slot="activator" color="primary" dark>New Container</v-btn>
+            <v-card>
+                <v-card-title>
+                    <span class="headline">New Container</span>
+                </v-card-title>
+                <v-divider/>
+                <error-alert :alert="implementVisible && formValid" text="Not implemented"/>
+                <error-alert :alert="!formValid" text="Please correct the errors and try again"/>
+                <v-card-text>
+                    <v-container grid-list-md>
+                        <v-layout wrap>
+                            <v-flex xs12>
+                                <v-form ref="form" v-model="formValid" lazy-validation>
+                                    <v-text-field 
+                                        v-model="studentID"
+                                        :rules="idRules"
+                                        :disabled="loading"
+                                        label="Student ID"
+                                        required/>
+                                </v-form>
+                            </v-flex>
+                        </v-layout>
+                    </v-container>
+                </v-card-text>
+                <v-divider/>
+                <v-card-actions>
+                    <v-spacer/>
+                    <v-btn color="primary" flat :disabled="loading" @click.native="closeDialog">Cancel</v-btn>
+                    <v-btn color="primary" flat :disabled="!formValid || loading" @click.native="createContainer">Create</v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
+        <progress-dialog v-model="loading" @visChange="onProgressChange"/>
+    </div>
 </template>
 
 <script>
@@ -41,6 +45,7 @@
             createContainer() {
                 // If the form is valid
                 if(this.$refs.form.validate()) {
+                    this.loading = true
                     this.implementVisible = true
                 }
             },
@@ -48,11 +53,15 @@
             closeDialog() {
                 this.showDialog = false
                 this.$refs.form.reset()
+            },
+            onProgressChange(val) {
+                this.loading = val
             }
         },
         data: () => ({
             studentID: '',
             showDialog: false,
+            loading: false,
             implementVisible: false,
             formValid: true,
             idRules: [
@@ -64,7 +73,8 @@
             ]
         }),
         components: {
-            'error-alert': () => import(/* webpackChunkName: "alertHelpers", webpackPrefetch: true */ './ErrorAlert.vue')
+            'error-alert': () => import(/* webpackChunkName: "alertHelpers", webpackPrefetch: true */ './ErrorAlert.vue'),
+            'progress-dialog': () => import(/* webpackChunkName: "dialogHelpers", webpackPrefetch: true */ './ProgressDialog.vue')
         }
     }
 </script>
---
+++ src/js/components/ProgressDialog.vue
@@ -0,0 +1,42 @@
+<template>
+    <v-dialog
+        v-model="visible"
+        persistent
+        width="300">
+        <v-card color="primary" dark>
+            <v-card-text>
+                Processing your request
+                <v-progress-linear
+                    indeterminate
+                    color="white"
+                    class="mb-0"/>
+            </v-card-text>
+        </v-card>
+    </v-dialog>
+</template>
+
+<script>
+    export default {
+        props: [
+            'visible'
+        ],
+        methods: {
+            updateVisibility(val) {
+                this.$emit('visChange', val)
+            }
+        },
+        model: {
+            prop: 'visible',
+            event: 'visChange'
+        },
+        watch: {
+            // Can't use () => or everything catches on fire
+            visible: function(val) {
+                if(!val) return
+                setTimeout(function() {
+                    this.updateVisibility(false)
+                }.bind(this), 4000)
+            }
+        }
+    }
+</script>
