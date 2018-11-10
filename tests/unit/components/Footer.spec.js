88f90bf855c3058f53870d7c185bfc75f62d3807
Joseph Zurowski <jzurowski2016@gmail.com>
2018-11-10 17:14:03 -0500

    Added Unit Tests for Footer

---
+++ tests/unit/components/Footer.spec.js
@@ -0,0 +1,30 @@
+import { mount } from '@vue/test-utils'
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import router from '@/router'
+import Footer from '@/components/Footer.vue'
+
+describe('Header', () => {
+    let wrapper
+
+    beforeEach(() => {
+        Vue.use(router)
+        Vue.use(Vuetify)
+
+        wrapper = mount(Footer, { router })
+    })
+
+    it('should render Vuetify components', () => {
+        expect(wrapper.contains('footer.v-footer')).toBe(true)
+    })
+
+    it('should render social media links correctly', () => {
+        wrapper.vm.icons = [
+            'fab fa-twitch',
+            'fab fa-google-plus',
+            'fab fa-steam'
+        ]
+
+        expect(wrapper.element).toMatchSnapshot()
+    })
+})
