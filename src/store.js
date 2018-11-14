ad4edbfc9541175c69f540a2bc5f2c20009cc59f
Joseph Zurowski <Kexerval@users.noreply.github.com>
2018-11-13 19:34:50 -0500

    Moved fetch() to vuex action

--- src/store.js
+++ src/store.js
@@ -1,21 +1,80 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import Kite from '@/mixins/api'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    currentContainer: '',
     containers: []
   },
   mutations: {
-      updateContainers(state, payload) {
-          state.containers = payload
+      updateContainers(state, containerList) {
+          state.containers = containerList
+      },
+      setCurrentContainer(state, id) {
+          state.containers = id
       }
   },
   actions: {
-      updateContainersAsync: ({ commit }, payload) => commit('updateContainers', payload)
+    updateContainersAsync: ({commit}) => {
+        return new Promise((resolve, reject) => {
+            const headers = new Headers()
+            const init = {
+                method: 'GET',
+                headers,
+                mode: 'cors',
+                cache: 'no-store'
+            }
+            
+            let request = new Request(`${Kite}api/docker`, init)
+
+            fetch(request).then((response) => {
+                // Check if response is okay and return data in JSON if true
+                if(response.ok) return response.json()
+                // If response not okay, reject the Promise with HTTP Status Code
+                else reject(response.status)
+            // This is where we filter some of the input out for better results
+            // on the frontend
+            }).then((parsedData) => {
+                // Perform data filtering on the JSON data
+                parsedData.forEach((element, elementIndex) => {
+                    let containerName = element.Name
+                    let containerStartTime = element.State.StartedAt
+
+                    // Filter out the junk Docker puts with the container name
+                    // We are filtering out the prefixed '/' and the postfixed 'php'
+                    // Use the first Regex result as we are evaluating each element separately
+                    containerName = containerName.match(/\b\d+/)[0]
+                    
+                    // Convert the Container Start Time from a UTC date string to a
+                    // Localized JavaScript Date object string
+                    containerStartTime = new Date(containerStartTime).toLocaleString()
+
+                    // Overwrite the parsedData with out new filtered data then repeat
+                    parsedData[elementIndex].Name = containerName
+                    parsedData[elementIndex].State.StartedAt = containerStartTime
+                })
+
+                // Return new parsedData to next Promise
+                return parsedData
+            }).then((filteredData) => {
+                // Commit to mutation
+                commit('updateContainers', filteredData)
+
+                // Resolve our completed promise
+                resolve()
+            // Catch any errors while retrieving API data
+            // such as an inability to connect to the server
+            }).catch(error => reject(error))
+        })
+    }
   },
   getters: {
+      currentContainer: (state) => {
+        return state.containers.find(container => container.Name === state.currentContainerId)
+      },
       getContainerByOwner: (state) => (owner) => {
           return state.containers.find(container => container.Name === owner)
       },
