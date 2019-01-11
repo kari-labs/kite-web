47657f744f6d4f6e99a92bb53123ebb3410372bc
Ian Fabs <ianfabs@outlook.com>
2019-01-11 13:42:31 -0500

    Removed un-used comments and function

--- src/plugins/kite/containers/containers.js
+++ src/plugins/kite/containers/containers.js
@@ -5,26 +5,7 @@ export default class Containers {
         this.api_url = config.api_url || '';
     }
 
-
-    /* mutation{
-  createContainer(userid: "chicken")
-} */
-
-    gql(args, ...values){
-        let query = "";
-        args.forEach( (string, i) => {
-            query += string + (values[i] || '');
-        });
-        return query.replace(/([\\][n])?([\s])+/g, " ");
-        //return JSON.stringify({query: query.replace(/([\\][n])?([\s])+/g, " ")})
-    }
-
     async createContainer(studentID) {
-        /* return await fetch(`${this.api_url}api/docker/${studentID}`, {
-            method: 'POST',
-            mode: 'cors',
-            cache: 'no-store'
-        }); */
         return fetch(`${this.api_url}api/graphql`, {
             method: 'POST',
             mode: 'cors',
@@ -42,11 +23,6 @@ export default class Containers {
     }
 
     async getContainers() {
-        /* return await fetch(`${this.api_url}api/docker`, {
-            method: 'GET',
-            mode: 'cors',
-            cache: 'no-store'
-        }); */
         return fetch(`${this.api_url}api/graphql`, {
             method: 'POST',
             mode: 'cors',
